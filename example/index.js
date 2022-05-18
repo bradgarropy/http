b8fe814fcb0e42ab8696439d7892810487e49d54
Brad Garropy <bgarropy@atlassian.com>
2022-05-17 22:56:06 -0500

    update example.

--- example/index.js
+++ example/index.js
@@ -1,11 +1,15 @@
 const {http} = require("../.")
 
 const main = async () => {
-    const posts = await http.get("https://jsonplaceholder.typicode.com/posts")
-    console.log(posts)
-
+    console.log("get post")
     const post = await http.get("https://jsonplaceholder.typicode.com/posts/1")
     console.log(post)
+
+    console.log("get posts")
+    const posts = await http.get("https://jsonplaceholder.typicode.com/posts")
+    console.table(posts, ["id", "userId", "title"])
+
+    console.log("filter posts")
 
     const userPosts = await http.get(
         "https://jsonplaceholder.typicode.com/posts",
@@ -14,7 +18,9 @@ const main = async () => {
         },
     )
 
-    console.log(userPosts)
+    console.table(userPosts, ["id", "userId", "title"])
+
+    console.log("create post")
 
     const newPost = await http.post(
         "https://jsonplaceholder.typicode.com/posts",
@@ -28,6 +34,27 @@ const main = async () => {
     )
 
     console.log(newPost)
+
+    console.log("update post")
+
+    const updatedPost = await http.patch(
+        "https://jsonplaceholder.typicode.com/posts/1",
+        {
+            body: {
+                title: "foo",
+            },
+        },
+    )
+
+    console.log(updatedPost)
+
+    console.log("delete post")
+
+    const deletedPost = await http.delete(
+        "https://jsonplaceholder.typicode.com/posts/1",
+    )
+
+    console.log(deletedPost)
 }
 
 main()
