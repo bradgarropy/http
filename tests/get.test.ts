9942398d77ffbb554b53f57d98108ee8d1db09ae
Brad Garropy <bgarropy@atlassian.com>
2022-05-17 22:58:26 -0500

    remove comments.

--- tests/get.test.ts
+++ tests/get.test.ts
@@ -12,33 +12,3 @@ test("uses correct method", () => {
     expect(mockHttpRequest).toHaveBeenCalledTimes(1)
     expect(mockHttpRequest).toHaveBeenCalledWith(mockUrl, "GET", mockOptions)
 })
-
-// const url = "https://jsonplaceholder.typicode.com/posts"
-
-// test("get all", async () => {
-//     const response = await httpGet<Post[]>(url)
-//     expect(response.slice(0, 10)).toEqual(mockPosts)
-// })
-
-// test("get one", async () => {
-//     const response = await httpGet<Post>(`${url}/1`)
-//     expect(response).toEqual(mockPosts[0])
-// })
-
-// test("get with headers", async () => {
-//     const options: Options = {
-//         headers: {"content-type": "application/json"},
-//     }
-
-//     const response = await httpGet<Post[]>(url, options)
-//     expect(response.slice(0, 10)).toEqual(mockPosts)
-// })
-
-// test("get with params", async () => {
-//     const options: Options = {
-//         params: {userId: 1},
-//     }
-
-//     const response = await httpGet<Post[]>(url, options)
-//     expect(response).toEqual(mockPosts)
-// })
